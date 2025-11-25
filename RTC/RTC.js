1eef081664d816f15cb90bf0391d56db2e5607f0
Nadol Hunchanasavi <116500822+newNKC@users.noreply.github.com>
2025-11-25 23:46:34 +0700

    Update RTC.js
    adjust ppp

--- RTC/RTC.js
+++ RTC/RTC.js
@@ -21,7 +21,7 @@ if (!statusEl) {
   if (form && form.parentNode) form.parentNode.insertBefore(statusEl, form.nextSibling);
 }
 
-const PRICE_PER_PERSON = 200;
+const PRICE_PER_PERSON = 1700;
 let bookingInfo = "";
 let pendingPayload = null;
 let hasSent = false;
